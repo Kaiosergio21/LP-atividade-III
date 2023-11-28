65e4032f8f737cc58737ab6611a6b3dde6f5f5e1
Kaio Sérgio Sales Nunes <137112682+Kaiosergio21@users.noreply.github.com>
2023-11-28 17:26:20 -0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int i, opcao;
+    char contatos[3][50]; 
+    int count;
+
+printf("quantidade de memória");
+scanf("%d",&count);
+
+    do {
+        printf("Escolha uma opção:\n");
+        printf("1. Adicionar Contato\n");
+        printf("2. Mostrar Contatos\n");
+        printf("3. Encerrar\n");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                for (i = 0; i < count; i++) {
+                    printf("Adicionar contato %d:\n", i + 1);
+                    scanf("%s", contatos[i]); 
+                }
+                break;
+
+            case 2:
+                for (i = 0; i < count; i++) {
+                    printf("Mostrar contato %d: %s\n", i + 1, contatos[i]);
+                }
+                break;
+
+            case 3:
+                printf("Encerrando o programa.\n");
+                break;
+
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 3);
+
+    return 0;
+}
