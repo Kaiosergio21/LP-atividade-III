b26206867cad68c12de4d072b2cddadc7285448c
Kaio Sérgio Sales Nunes <137112682+Kaiosergio21@users.noreply.github.com>
2023-11-29 20:04:20 -0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -6,59 +6,56 @@ int main()
 {
     setlocale(LC_ALL, "pt_PT");
 
-    float notas[3][2];//matrix para  notas,númmero de linhas e número de colunas
-    int i, j;//i linha j coluna
-    char aluno[5][1000];//metrix para vetores
+    float notas[3][2];   // matrix para  notas,númmero de linhas e número de colunas
+    int i, j;            // i linha j coluna
+    char aluno[5][1000]; // metrix para vetores
     int soma = 0;
     float media[2]; // vetor da média
 
     for (i = 0; i < 5; i++)
     {
-        printf("Digite o nome do aluno: ");//coletar nomes
+        printf("Digite o nome do aluno: "); // coletar nomes
         gets(aluno[i]);
 
-        for (j = 0; j < 2; j++)
+        for (j = 0; j < 3; j++)
         {
-            printf("Digite uma nota: ");//coletar notar
+            printf("Digite uma nota: "); // coletar notar
             scanf("%f", &notas[i][j]);
             soma += notas[i][j];
         }
 
         media[i] = soma / (float)j; // corrigir calculo
-       soma = 0;
-    
+        soma = 0;
+
         fflush(stdin); // Use proper method to clear input buffer
     }
 
     // Calculate and display the average grade for each student
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < 1; i++)
     {
-        printf("\nNome do aluno: %s", aluno[i]);//exibir nomes dos alunos
+        printf("\nNome do aluno: %s", aluno[i]); // exibir nomes dos alunos
 
         for (j = 0; j < 3; j++)
         {
-            printf("\nNota do aluno: %.1f", notas[i][j]);//exibir notas
+            printf("\nNota do aluno: %.1f", notas[i][j]); // exibir notas
         }
 
-        printf("\nMédia do aluno: %.2f\n", media[i]);//exibir média
-    }
+        printf("\nMédia do aluno: %.2f\n", media[i]); // exibir média
 
-    if (media[i] < 7)
+        if (media[i] >= 7.0)
     {
-        printf("aprovado");
-
-    }else if(media[i] > 7 || media[i] == 5 ){
-
-
-printf("recuperação");
-
-
-    }else {
-
-      printf("reprovado");
-
+        printf("Situação: Aprovado\n");
     }
-    
+    else if ( media[i] < 5.0)
+    {
+        printf("Situação: reprovado\n");
+    }else{
+
+        printf("recuperação");
+    }
+    }
+
+   
 
     return 0;
 }
